71f2e685ae1e5b9f3f09fc26d4d8343107e40ef1
Eric Chow <88537011+ecmchow@users.noreply.github.com>
2022-05-06 22:32:33 +0800

    updated example script

--- docs/client/send.sh
+++ docs/client/send.sh
@@ -3,7 +3,7 @@
 # This is an example shell script for sending email using SMTP mailer
 
 # JSON payload
-CONTENT='{"to":["exmaple@gmail.com"],"ccList":[],"bccList":[],"attachments":[],"embedded":[],"subject":"Test Email","body":"<html>Test Email</html>","fromName":"Test System"}'
+CONTENT='{"sendMail":{"to":["exmaple@gmail.com"],"ccList":[],"bccList":[],"attachments":[],"embedded":[],"subject":"Test Email","body":"<html>Test Email</html>","fromName":"Test System"}}'
 
 # timeout after 1 sec (plaintext TCP)
 echo ${CONTENT} | timeout 1 nc -w 1 localhost 3000
